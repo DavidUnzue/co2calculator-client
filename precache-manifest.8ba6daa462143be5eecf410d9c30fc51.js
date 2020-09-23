53c2865a0b4df1b952445f75ae68bd86fa3fbe2e
David Unzue <davidunzue@gmail.com>
2020-09-23 15:09:58 +0200

    Updates

---
+++ precache-manifest.8ba6daa462143be5eecf410d9c30fc51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb608888217c7e0ca4bc4c0c9b226865",
+    "url": "/co2calculator-client/index.html"
+  },
+  {
+    "revision": "b4a2dd1c52410776d261",
+    "url": "/co2calculator-client/static/css/main.18b0c9fb.chunk.css"
+  },
+  {
+    "revision": "957dd5b3e655b266aa17",
+    "url": "/co2calculator-client/static/js/2.adfc404e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/co2calculator-client/static/js/2.adfc404e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a2dd1c52410776d261",
+    "url": "/co2calculator-client/static/js/main.616734b0.chunk.js"
+  },
+  {
+    "revision": "b8c9a2638baf70ac10d0",
+    "url": "/co2calculator-client/static/js/runtime-main.f4c4f5d3.js"
+  },
+  {
+    "revision": "e26b195650315dacaeafa702e440c4d2",
+    "url": "/co2calculator-client/static/media/car.e26b1956.svg"
+  },
+  {
+    "revision": "eb5ed7f2f7e1f080c4287748ee881794",
+    "url": "/co2calculator-client/static/media/plane.eb5ed7f2.svg"
+  },
+  {
+    "revision": "43c0562f63fd6632786b6624da594d6f",
+    "url": "/co2calculator-client/static/media/train.43c0562f.svg"
+  }
+]);
